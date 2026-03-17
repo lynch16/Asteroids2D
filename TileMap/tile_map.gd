f095f95036c1e432807477ad81dd2a565051aea1
Will Lynch <will.lynch91@gmail.com>
2026-03-17 13:02:56 -0400

    Get edge tracking working 80%

--- TileMap/tile_map.gd
+++ TileMap/tile_map.gd
@@ -2,25 +2,44 @@
 class_name TileMap_MSquares
 extends Node2D
 
+const CORNER_NW = Vector2(-1,1);
+const CORNER_NE = Vector2(1,1);
+const CORNER_SE = Vector2(1,-1);
+const CORNER_SW = Vector2(-1,-1);
+
 const CORNERS: PackedVector2Array = [
-	Vector2(-1, 1),
-	Vector2(1, 1),
-	Vector2(1, -1),
-	Vector2(-1, -1),
+	CORNER_NW,
+	CORNER_NE,
+	CORNER_SE,
+	CORNER_SW,
+];
+const NEXT_CHECK: PackedVector2Array = [
+	Vector2.UP,
+	Vector2.RIGHT,
+	Vector2.DOWN,
+	Vector2.LEFT
 ]
-const TILE_SIZE = 16;
+const TILE_SIZE = 32;
 const HALF_TILE_SIZE = TILE_SIZE/2;
 const BLOCK_DIVISION = 0.5;
 const DOT_BUTTON_RADIUS = 2.0;
 const ALL_CORNERS = 15 # 1111 in binary
+const NO_CORNERS = 0 # 0000 in binary
 
 @export var noise: FastNoiseLite;
 @export_tool_button("Generate", "Callable") var generate := _sample_viewport;
 
-var corner_sample_tracker: Dictionary[Vector2, int] = {};
+var corner_sample_tracker: Dictionary[Vector2, float] = {};
 var surface_array := [];
 var dots_lookup: Dictionary[Vector2, bool] = {};
 var last_corner_hover: Vector2;
+var polygons := [];
+var mdt: MeshDataTool;
+var centers_lookup: Dictionary[Vector2, int] = {};
+var tracked_verts: Dictionary[Vector2, int] = {};
+
+var recursion := 0;
+var max_recur := 500;
 	
 func _ready() -> void:
 	InputMap.load_from_project_settings()
@@ -31,24 +50,106 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	queue_redraw()
 	
+	($Label as Label).text = str(get_viewport().get_mouse_position());
+	
 	# TODO: Put this into a window to limit interactions until in use
 	# TODO: Save resulting mesh as resource
 	# TODO: Add Collision
+	# TODO: Make dots a separate resource to limit redraws
 	if Input.is_action_just_pressed("left_click"):
 		_handle_mouse_click();
+	
+	if Input.is_action_just_pressed("right_click"):
+		_handle_mouse_right_click();
+
+func _track_verts(tile_center: Vector2) -> void:
+	tracked_verts.set(tile_center, true);
+	recursion += 1;
+	if (recursion > max_recur): return;
+	
+	var viewport_rect := get_viewport_rect();
+	var tile_index := centers_lookup[tile_center];
+
+	if (tile_index == ALL_CORNERS):
+		if (
+			(tile_center.x + TILE_SIZE > viewport_rect.size.x) || \
+			(tile_center.y + TILE_SIZE > viewport_rect.size.y) || \
+			(tile_center.x - TILE_SIZE < 0) || \
+			(tile_center.y - TILE_SIZE < 0)
+		):
+			print("INCLUDE");
+			# TODO: Edges not yet included automatically. The tile walker never moves into this block
+		print("ALL_CORNERS");
+	elif (tile_index == NO_CORNERS):
+		print("NO CORNERS");
+	else:
+		var next_tile_dirs := MSMeshes.NEXT_VERTEX_ARRAYS[tile_index];
+		for i in next_tile_dirs.size():
+			var next_tile := next_tile_dirs[i] * TILE_SIZE + tile_center;
+			if (viewport_rect.has_point(next_tile) && !tracked_verts.has(next_tile)):
+				_track_verts(next_tile);
+
+func _get_next_edge(tile_index: int, tile_center: Vector2) -> Vector2:
+	var next_tile_dirs := MSMeshes.NEXT_VERTEX_ARRAYS[tile_index];
+	for i in next_tile_dirs.size():
+		var next_tile := next_tile_dirs[i] * TILE_SIZE + tile_center;
+		var viewport_rect := get_viewport_rect();
+		if (viewport_rect.has_point(next_tile) && !tracked_verts.has(next_tile)):
+			return next_tile;
+	
+	return Vector2(-1.0, -1.0);
+
+func _handle_mouse_right_click() -> void:
+	var mouse_down_position := get_viewport().get_mouse_position();
+	var tile_center := _get_position_tile_center_coord(mouse_down_position);
+	_track_verts(tile_center);
+	
+		# Null = END
+		# Only SW == One down, one left
+		# Only SE == One Down, one right
+		# SW + SE == One left, one right (NO DOWN AS FULL)
+		# Only NE == One Up, One right
+		# NE + SW == One Up, One right, one down, one left
+		# NE+SE = One Up, one down, (NO RIGHT AS FULL)
+		# NE, SE, SW = One UP, one left (NO DOWN or RIGHT AS FULL)
+		# Only NW = One UP, ONe left
+		# NW + SW = One Up, One down (NO LEFT AS FULL)
+		# NW + SW = One Up, One RIght, One Down, One left
+		# NW, SW, SE = One Up, One Right (NO DOWN OR LEFT AS FULL)
+		# NW + NE = One Right, One Left
+		# NW, NE, SW = One Right, One Down (NO UP OR LEFT AS FULL)
+		# NW, NE, SE = One Left, One Down (NO UP OR RIGHT AS FULL)
+		# ALL FULL = SKIP
+		
+		
+	# TODO: Open a mouse dropdown to select the action
+	# 1. Track what type of tile each tile is
+	# 2. When selecting a tile, get the tile type,
+			# If tile type is all 4 corners, walk tiles until not
+	# 3. use the tile type to get the normalized verticies, 
+	# 4. use the normalized verticies to determine edges
+	# 5. save edge verticies to a new PackedVector2Array
+	# 6. 
+	# TMP: Create a collision polygon
+	pass;
 				
 func _handle_mouse_click() -> void:
 	var mouse_down_position := get_viewport().get_mouse_position();
 	var corner := _get_position_tile_corner_coord(mouse_down_position);
 	
 	if (corner_sample_tracker.has(corner)):
-		var curr_val: int = corner_sample_tracker.get(corner);
+		var curr_val: int = _get_sample_int_from_vertex(corner);
 		corner_sample_tracker.set(corner, int(curr_val == 0));
 		_generate_mesh();
-	
-func _get_position_tile_corner_coord(mouse_position: Vector2) -> Vector2:
+		
+func _get_position_tile_center_coord(mouse_position: Vector2) -> Vector2:
 	var tile_pos := Vector2(floori(mouse_position.x / TILE_SIZE), floori(mouse_position.y / TILE_SIZE)); 
 	var center := Vector2(tile_pos.x + BLOCK_DIVISION, tile_pos.y + BLOCK_DIVISION) * float(TILE_SIZE);
+
+	return center;
+		
+func _get_position_tile_corner_coord(mouse_position: Vector2) -> Vector2:
+	var center := _get_position_tile_center_coord(mouse_position);
 	
 	for i: int in CORNERS.size():
 		var corner := center + CORNERS[i] * float(HALF_TILE_SIZE);
@@ -66,7 +167,7 @@ func _save_corner_samples(center: Vector2) -> void:
 	for i: int in CORNERS.size():
 		var corner := center + CORNERS[i] * float(HALF_TILE_SIZE);
 		if (!corner_sample_tracker.has(corner)):
-			corner_sample_tracker.set(corner, int(_sample(corner) > 0.0));
+			corner_sample_tracker.set(corner, _sample(corner));
 	
 func _sample(input: Vector2) -> float:
 	return noise.get_noise_2dv(input);
@@ -88,18 +189,30 @@ func _generate_mesh() -> void:
 	
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array);
 	
+	mdt = MeshDataTool.new();
+	mdt.create_from_surface(
+		($MeshInstance2D as MeshInstance2D).mesh as ArrayMesh,
+		0
+	);
+
 func _generate_tile_from_corners(center: Vector2) -> void:
 	var tile_index := 0;
 	for i: int in CORNERS.size():
 		var corner := center + CORNERS[i] * float(HALF_TILE_SIZE);
 		if (!corner_sample_tracker.has(corner)):
 			continue;
-					
-		tile_index += corner_sample_tracker.get(corner) << i;
+		
+		tile_index += _get_sample_int_from_vertex(corner) << i;
 				
 	_add_tile(center, tile_index);
+	
+func _get_sample_int_from_vertex(corner_vector: Vector2) -> int:
+	var corner_sample: float = corner_sample_tracker.get(corner_vector);
+	return int(corner_sample > 0.0)
 
 func _add_tile(center: Vector2, tile_index: int) -> void:
+	centers_lookup[center] = tile_index;
+	
 	var texture: CompressedTexture2D = ($MeshInstance2D as MeshInstance2D).texture;
 	var verticies := MSMeshes.VERTEX_ARRAYS[tile_index].duplicate();
 	var indicies := MSMeshes.INDEX_ARRAYS[tile_index].duplicate();
@@ -110,8 +223,7 @@ func _add_tile(center: Vector2, tile_index: int) -> void:
 	
 		for i: int in verticies.size():
 			var vertex := verticies[i];
-			vertex = vertex * float(HALF_TILE_SIZE) + center;
-			verticies[i] = vertex;
+			verticies[i] = _calculate_weighted_vertex(center, vertex);
 	
 		array_vertex.append_array(verticies);
 	
@@ -136,30 +248,56 @@ func _add_tile(center: Vector2, tile_index: int) -> void:
 			indicies[i] += total_verticies;
 		
 		array_index.append_array(indicies);
-		
+
+func _calculate_weighted_vertex(center: Vector2, normalized_vertex: Vector2) -> Vector2:
+	var is_edge := absf(normalized_vertex.x) + absf(normalized_vertex.y) == 1.0;
+	var direction := Vector2.UP if normalized_vertex.x != 0.0 else Vector2.RIGHT;
+			
+	var vertex := normalized_vertex * float(HALF_TILE_SIZE) + center;
+	#if (is_edge):
+		#var corner_1 := vertex + direction * HALF_TILE_SIZE;
+		#var corner_2 := vertex - direction * HALF_TILE_SIZE;
+		#var value_1: float = corner_sample_tracker.get(corner_1);
+		#var value_2: float = corner_sample_tracker.get(corner_2);
+		#var weight := (value_1 + value_2)/(value_1 - value_2) / 2.0 + 0.5;
+		#vertex = corner_1.lerp(corner_2, weight);
+	
+	return vertex;
+
 # Draw dots at each vertex, colored whether the mouse is hovering
 func _draw() -> void:
 	_draw_dots()
+	for k: Vector2 in tracked_verts.keys():
+		var rect := Rect2(k.x - HALF_TILE_SIZE, k.y - HALF_TILE_SIZE, TILE_SIZE, TILE_SIZE);
+		draw_rect(rect, Color.ORANGE, false);
 		
 func _draw_dots() -> void:
 	dots_lookup = {};
 	_for_each_tile(_draw_dot);
 	_track_mouse_hover();
+
+func _draw_corner_dot(corner: Vector2) -> void:
+	if (dots_lookup.has(corner) || !corner_sample_tracker.has(corner)):
+		return;
+			
+	var color := Color.RED;
+	var corner_sample: int = _get_sample_int_from_vertex(corner);
+	if (corner_sample > 0):
+		color = Color.GREEN;
+		
+	if (corner == last_corner_hover):
+		color = Color.BLUE;
+			
+	dots_lookup.set(corner, _get_sample_int_from_vertex(corner));
+	draw_circle(corner, DOT_BUTTON_RADIUS, color);
+
+func _draw_center_dot(center: Vector2) -> void:
+	draw_circle(center, DOT_BUTTON_RADIUS, Color.PURPLE);
 	
 func _draw_dot(center: Vector2) -> void:
-	for i: int in CORNERS.size():
-		var corner := center + CORNERS[i] * float(HALF_TILE_SIZE);
-		if (dots_lookup.has(corner)):
-			return;
-			
-		var color := Color.RED;
-		
-		if (corner == last_corner_hover):
-			color = Color.GREEN;
-			
-		dots_lookup.set(corner, true);
-		draw_circle(corner, DOT_BUTTON_RADIUS, color);
-		
+	_draw_center_dot(center);
+	_for_each_corner(center, _draw_corner_dot)
+
 func _track_mouse_hover() -> void:
 	var mouse_position := get_viewport().get_mouse_position();
 	var corner := _get_position_tile_corner_coord(mouse_position);
@@ -181,3 +319,8 @@ func _for_each_tile(callable: Callable) -> void:
 		for y: int in range(0, y_viewport_tiles + 1):
 			var center := Vector2(x + BLOCK_DIVISION, y + BLOCK_DIVISION) * float(TILE_SIZE);
 			callable.call(center);
+			
+func _for_each_corner(center: Vector2, callable: Callable) -> void:
+	for i: int in CORNERS.size():
+		var corner := center + CORNERS[i] * float(HALF_TILE_SIZE);
+		callable.call(corner);
