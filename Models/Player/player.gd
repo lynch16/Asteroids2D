3e3fd6a1e0d43412b4d32224075577dc0ada9956
Will Lynch <will.lynch91@gmail.com>
2026-05-06 14:28:33 -0400

    Add sound and animation to player thurst

--- Models/Player/player.gd
+++ Models/Player/player.gd
@@ -3,7 +3,11 @@ extends CharacterBody2D
 
 @export var movement_stats: MovementStats;
 @export var combat_stats: CombatStats;
+
 @onready var hurtbox: Hurtbox2D = %Hurtbox2D;
+@onready var thrust_animations: ThrustAnimations = get_node("AnimatedShipSprite2D/ThrustAnimations");
+@onready var thrust_sound: AudioStreamPlayer = get_node("ThrustSound");
+@onready var turn_sound: AudioStreamPlayer = get_node("TurnSound");
 
 var damageable: Damageable;
 var ship_direction: float;
@@ -27,11 +31,14 @@ func _ready() -> void:
 	
 func _physics_process(delta: float) -> void:
 	acceleration = Vector2.ZERO;
+	var is_turning := false;
 	
 	if (Input.is_action_pressed("yaw_left")):
 		_yaw_left(delta);
+		is_turning = true;
 	
 	if (Input.is_action_pressed("yaw_right")):
+		is_turning = true;
 		_yaw_right(delta);
 	
 	if (Input.is_action_pressed("thrust")):
@@ -42,12 +49,35 @@ func _physics_process(delta: float) -> void:
 		
 	var tmp_vel := velocity + (acceleration * delta);
 	velocity = tmp_vel.min(Vector2(movement_stats.max_speed, movement_stats.max_speed));
-	
+
 	if (!ship_direction):
 		ship_direction = rotation;
 	rotation = ship_direction;
-	
+
+	_handle_animation_and_sound(
+		acceleration.length() > 0.0,
+		is_turning
+	);
+
 	move_and_slide();
+
+func _handle_animation_and_sound(
+	is_accelerating: bool,
+	is_turning: bool
+) -> void:
+	if (is_accelerating):
+		thrust_animations.start_animation();
+		if (!thrust_sound.playing):
+			thrust_sound.play();
+	else:
+		thrust_animations.stop_animation();
+		thrust_sound.stop();
+
+	if (is_turning):
+		if (!turn_sound.playing):
+			turn_sound.play();
+	else:
+		turn_sound.stop();
 
 func _handle_player_damage(_old_health: float, new_health: float) -> void:
 	SignalBus._on_player_health_updated(int(new_health));
