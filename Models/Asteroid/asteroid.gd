ac1b6acd0b51a993decf0861952220da4192ddb2
Will Lynch <will.lynch91@gmail.com>
2026-05-28 22:29:21 -0400

    Add audio and particles for asteroid death

--- Models/Asteroid/asteroid.gd
+++ Models/Asteroid/asteroid.gd
@@ -6,6 +6,9 @@ class_name Asteroid extends SpawnableCharacter2D
 @export var health_stats: HealthStats;
 @export var mesh_deformation_shapes: Array[MeshDeformationShape] = [];
 
+@onready var death_particles: GPUParticles2D = $DeathParticles2D;
+@onready var death_audio_player: AudioStreamPlayer2D = $DeathAudioStreamPlayer2D;
+
 var mass := 10000;
 var damageable: Damageable;
 var safe_collision_time := 5.0;
@@ -18,8 +21,22 @@ func _enter_tree() -> void:
 	if (!hurtbox):
 		hurtbox = %MeshDeformHitHurtbox2D;
 
-	if hurtbox is MeshDeformHurtbox2D:
-		var deformable_hurtbox: MeshDeformHurtbox2D = hurtbox;
+	if hurtbox is MeshDeformHitHurtbox2D:
+		var deformable_hurtbox: MeshDeformHitHurtbox2D = hurtbox;
+
+		# Propagate stats so only manage at top level
+		deformable_hurtbox.combat_stats = combat_stats;
+		deformable_hurtbox.health_stats = health_stats;
+	
+func _ready() -> void:
+	add_to_group("enemy");
+	health_stats.resource_local_to_scene = true;
+	combat_stats.resource_local_to_scene = true;
+
+	if hurtbox is MeshDeformHitHurtbox2D:
+		var deformable_hurtbox: MeshDeformHitHurtbox2D = hurtbox;
+
+		# Connect to mesh deformation results
 		deformable_hurtbox.spawn_new_group.connect(_shatter);
 		deformable_hurtbox.all_colliders_destroyed.connect(_destroy);
 
@@ -28,11 +45,6 @@ func _enter_tree() -> void:
 	invincible_damage_result.end.connect(_enable_colliders);
 
 	health_stats.on_health_depleted.connect(_destroy);
-	
-func _ready() -> void:
-	add_to_group("enemy");
-	health_stats.resource_local_to_scene = true;
-	combat_stats.resource_local_to_scene = true;
 
 func _physics_process(_delta: float) -> void:
 	# Clamp velocity to reasonable playable value
@@ -59,9 +71,22 @@ func _enable_colliders() -> void:
 			collision.set_deferred("disabled", false);
 
 func _destroy() -> void:
-	call_deferred("queue_free");
+	if (is_inside_tree()):
+		death_particles.emitting = true;
+		death_audio_player.playing = true;
+
+	var timer := Timer.new();
+	timer.wait_time = death_particles.lifetime;
+	timer.autostart = true;
+	add_child.call_deferred(timer);
+	timer.timeout.connect(_on_dequeue_timeout)
+
 	asteroid_destroyed.emit();
-	# TODO: Destroy animation
+
+func _on_dequeue_timeout() -> void:
+	death_audio_player.playing = false;
+	death_particles.emitting = false;
+	call_deferred("queue_free");
 
 func deform_mesh(collision_point: Vector2, collision_angle: float, collision_deformation_shapes: Array[MeshDeformationShape]) -> void:
 	if hurtbox is MeshDeformHurtbox2D:
