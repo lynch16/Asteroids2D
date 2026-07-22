0d6bc5471597bd4d8a1e3cd774ad087146b125e1
Will Lynch <will.lynch91@gmail.com>
2026-07-22 11:11:35 -0400

    Dont couple generator with editor

--- TileMap/MarchingSquares/NEW/ms_editor.gd
+++ TileMap/MarchingSquares/NEW/ms_editor.gd
@@ -1,2 +1,8 @@
 class_name MS_Editor extends Node2D 
 
+@onready var generator: MS_Generator = $MS_Generator;
+@onready var ms_canvas_2d: MS_Canvas2D = $MS_Canvas2D;
+
+func _ready() -> void:
+	generator.canvas = ms_canvas_2d.canvas;
+	generator.ms_bitmap = ms_canvas_2d.ms_bitmap;
--- TileMap/MarchingSquares/NEW/ms_generator.gd
+++ TileMap/MarchingSquares/NEW/ms_generator.gd
@@ -1,15 +1,14 @@
 class_name MS_Generator extends Node2D
 
-@export var ms_canvas_2D: MS_Canvas2D;
+@export var canvas: MS_Canvas;
+@export var ms_bitmap: MS_Bitmap;
 @export var texture: Texture2D;
 
-signal shape_generated(shape: ConvexPolygonShape2D);
-
 func generate_collision_shapes() -> void:
-	ms_canvas_2D.ms_bitmap.shrink();
-	ms_canvas_2D.canvas.resize(ms_canvas_2D.ms_bitmap.get_size() * ms_canvas_2D.canvas.tile_size);
+	ms_bitmap.shrink();
+	canvas.resize(ms_bitmap.get_size() * canvas.tile_size);
 	
-	var collision_polygons := ms_canvas_2D.ms_bitmap.to_polygon_shapes(ms_canvas_2D.canvas);
+	var collision_polygons := ms_bitmap.to_polygon_shapes(canvas);
 
 	for polygon in collision_polygons:
 		var collision := CollisionShape2D.new();
@@ -17,8 +16,8 @@ func generate_collision_shapes() -> void:
 		add_child(collision);
 
 	var mesher := MS_Mesh.new(
-		ms_canvas_2D.ms_bitmap,
-		ms_canvas_2D.canvas,
+		ms_bitmap,
+		canvas,
 		texture
 	);
 
